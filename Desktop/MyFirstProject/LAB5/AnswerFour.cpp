96873f086e9ce93081e41f6cfdca27828faff5a3
bed-com-32-21@unima.ac.mw <bed-com-32-21@unima.ac.mw>
2024-04-30 02:40:18 +0200

    experienced a problem failing to add other files

--- Desktop/MyFirstProject/LAB5/AnswerFour.cpp
+++ Desktop/MyFirstProject/LAB5/AnswerFour.cpp
@@ -1,20 +1,34 @@
-
+/*
+As an output to the console, a program prompt the user for an integer value 
+between 5 and 10.
+solution ensure that the input is a valid representation of an integer
+conditions: if the integer value is not between 5 and 10, output to the console a statement to prompt the user for an integer value between 5 and 
+10.
+ensure that the integer value is between 5 and 10 before exiting the 
+iteration.
+After the loop iteration code block, output to the console a statement to inform the user that their input value has been accepted
+*/
 #include<iostream>
 #include <limits>
 using namespace std;
 
 int main(){
     int input;
-    cout<<"Enter a value between 5 and 10: ";
-    cin>>input;
 
-    while(!(input >= 5 && input <= 10)){
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout<<"\nSorry You have entered an invalid number please try again"<<endl;
-        cin>>input;
-    }
+       do {
+        //Enter a value between 5 and 10: ";
+        cout << "Enter a value between 5 and 10: ";
+        // prompts the user to enter integer
+        cin >> input;
 
+        // Clear the input stream and ignore any remaining characters if input is invalid
+        if (!(input >= 5 && input <= 10)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "\nSorry, you have entered an invalid number. Please try again.\n";
+        }
+    } while (!(input >= 5 && input <= 10));
+    
     printf("Your input value(%d) has been accepted\n", input);
     return 0;
 }
